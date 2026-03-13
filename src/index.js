f6e1b40f07106c0c24081e42ca3def89e7516922
Sindi <londisotrading@gmail.com>
2026-03-13 20:40:01 +0200

    search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    console.log(response.data);
+
+}
+    function searchCity(city) {
+    let apiKey = "643fa0b05b03a4a4ca8b0odtc3e69d42";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-cityElement.innerHTML=searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
+
+searchCity("Paris");
